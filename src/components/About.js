04ccecb0f47e43acd455700d1862f378050e0b6d
amelia.risner0@gmail.com <arisner@berkeley.edu>
2022-02-01 18:16:27 -0800

    commit tree b11315b0998a4f06e3399b13ae219d8acba4ffd3

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,14 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>Proud owner of an imaginary cat</p>
+    <img 
+    src={image}
+    alt="I made this"
+    />
+  </div>;
 }
 
 export default About;
